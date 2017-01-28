90ae762c4411c1d2fc4ac6aaa5a9efb63601b0f9
cwk8f <cwk8f@cwk8f-retina.local>
2017-01-27 22:10:41 -0500

    fixed links on home page

--- home.php
+++ home.php
@@ -22,16 +22,20 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
         <h1>The Ultimate Fundraising Experience<sup>&trade;</sup></h1>
         <div class="row">
         	<div class="col-md-6 cell">
-						<a class="btn" href="template-form-campaign-start.html" role="button">
+						<a class="btn" href="request-a-fundraiser" role="button">
 							<img class="" src="<?php echo get_theme_file_uri( 'img/home-jumbotron-start.svg' );?>" alt="" title="" />
 						</a>
-        		<p><a class="btn btn-primary btn-lg btn-home" href="template-form-campaign-start.html" role="button">Start a Campaign</a></p>
+        		<p>
+        		  <a class="btn btn-primary btn-lg btn-home" href="request-a-fundraiser" role="button">Start a Campaign</a>
+        		</p>
         	</div> <!-- /column -->
         	<div class="col-md-6 cell">
-						<a class="btn" href="template-page-campaign-find.html" role="button">
+						<a class="btn" href="organizations" role="button">
 							<img class="" src="<?php echo get_theme_file_uri( 'img/home-jumbotron-find.svg' );?>" alt="" title="" />
 						</a>
-        		<p><a class="btn btn-primary btn-lg btn-home" href="template-page-campaign-find.html" role="button">Find a Campaign</a></p>
+        		<p>
+        		  <a class="btn btn-primary btn-lg btn-home" href="organizations" role="button">Find a Campaign</a>
+        		</p>
         	</div> <!-- /column -->
         </div> <!-- /row -->
         <div class="alert" role="alert">
@@ -53,7 +57,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
       <div class="row campaign-feature">
 
         <div class="col-md-3">
-        	<a class="no-line" href="template-page-campaign-home.html">
+        	<a class="no-line" href="#">
 						<div class="panel panel-default campaign">
 							<div class="panel-body">
 								<img src="<?php echo get_theme_file_uri( 'img/img.png' );?>" alt="" title="" width="100%" height="100%" />
@@ -69,7 +73,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 					</a>
         </div> <!-- /column -->
         <div class="col-md-3">
-        	<a class="no-line" href="template-page-campaign-home.html">
+        	<a class="no-line" href="#">
 						<div class="panel panel-default campaign">
 							<div class="panel-body">
 								<img src="<?php echo get_theme_file_uri( 'img/img.png' );?>" alt="" title="" width="100%" height="100%" />
@@ -85,7 +89,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 					</a>
         </div> <!-- /column -->
         <div class="col-md-3">
-        	<a class="no-line" href="template-page-campaign-home.html">
+        	<a class="no-line" href="#">
 						<div class="panel panel-default campaign">
 							<div class="panel-body">
 								<img src="<?php echo get_theme_file_uri( 'img/img.png' );?>" alt="" title="" width="100%" height="100%" />
@@ -101,7 +105,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 					</a>
         </div> <!-- /column -->
         <div class="col-md-3">
-        	<a class="no-line" href="template-page-campaign-home.html">
+        	<a class="no-line" href="#">
 						<div class="panel panel-default campaign">
 							<div class="panel-body">
 								<img src="<?php echo get_theme_file_uri( 'img/img.png' );?>" alt="" title="" width="100%" height="100%" />
@@ -138,7 +142,6 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 						<div class="panel-body">
 							<div class="step-num">1</div>
 							<img class="step-img" src="<?php echo get_theme_file_uri( 'img/home-howitworks-1.svg' );?>" alt="" title="" />
-							<h2>Heading</h2>
 							<p>Once an organization has signed up to utilize UFund, we will create an online store just for that organization or group. This website will be branded to look just as if it is that group’s website. The group’s website will keep track of how much the group has raised and will let everyone know with a tracker at the top of each page. Your organization receives a check for 25% of the total sales price! That’s $25 out of every $100 sold.</p>
 						</div>
 					</div>
@@ -148,7 +151,6 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 						<div class="panel-body">
 							<div class="step-num">2</div>
 							<img class="step-img" src="<?php echo get_theme_file_uri( 'img/home-howitworks-2.svg' );?>" alt="" title="" />
-							<h2>Heading</h2>
 							<p>Once the group’s UFund website is up and running, UFund will release the URL address to the organization. Then all of the organization’s members will be asked to share that particular website with all of their friends and family via social media. The more people each member shares it with, the more money the group will raise.</p>
 						</div>
 					</div>
@@ -158,7 +160,6 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 						<div class="panel-body">
 							<div class="step-num">3</div>
 							<img class="step-img" src="<?php echo get_theme_file_uri( 'img/home-howitworks-3.svg' );?>" alt="" title="" />
-							<h2>Heading</h2>
 							<p>Each fundraiser lasts for 2 weeks (and can be extended for a week if the organization requests it). The fundraiser will then close, and UFund will give the group a detailed report on all purchases made, so your fundraising team can celebrate! At the time of checkout, each customer will provide their mailing address and their products will be shipped directly to them. UFund will take 100% of the work out of having to deliver any products!</p>
 						</div>
 					</div>
@@ -168,7 +169,6 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 						<div class="panel-body">
 							<div class="step-num">4</div>
 							<img class="step-img" src="<?php echo get_theme_file_uri( 'img/home-howitworks-4.svg' );?>" alt="" title="" />
-							<h2>Heading</h2>
 							<p>UFund will calculate the amount of funds raised and will distribute a check to the organization within 2-4 weeks of the fundraiser’s last day. In certain circumstances, UFund will host a success party for the organization as well. If you have any questions, please contact us at info@UFundonline.com. UFund looks forward to working with your group or organization in the near future.</p>
 						</div>
 					</div>
